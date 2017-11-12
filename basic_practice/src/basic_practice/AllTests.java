a0a0804f67d7480f1c612dcb6d3a0667cc20c5c7
baozhiming <970967681@qq.com>
2017-11-13 00:23:58 +0800

    last checkout code

--- basic_practice/src/basic_practice/AllTests.java
+++ basic_practice/src/basic_practice/AllTests.java
@@ -7,5 +7,4 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ test1.class, test2.class, test5.class, test6.class, test7.class, test8.class })
 public class AllTests {
-
 }
--- basic_practice/src/basic_practice/WAllTests.java
+++ basic_practice/src/basic_practice/WAllTests.java
@@ -7,5 +7,6 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ testw1.class, testw2.class, testw3.class, testw4.class, testw5.class })
 public class WAllTests {
+	
 
 }
